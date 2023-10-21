a551235b9625e2a43a224955ea7cf803d935c94d
Kipngenocodes <kipngenoalex.c@gmail.com>
2023-10-21 14:17:23 -0600

    color added

---
+++ py.py
@@ -0,0 +1,42 @@
+import tkinter as tk
+
+
+# Function to assign work based on age
+def assign_work():
+    age = int(age_entry.get())
+    if age < 18:
+        work = "Student"
+        text_color = "blue"
+    elif age < 30:
+        work = "Junior"
+        text_color = "green"
+    elif age < 50:
+        work = "Mid-level"
+        text_color = "orange"
+    else:
+        work = "Senior"
+        text_color = "red"
+
+    work_label.config(text=f"Work: {work}", fg=text_color)
+
+
+# Create the tkinter window
+window = tk.Tk()
+window.title("Work Assignment")
+
+# Create labels and entry widget
+age_label = tk.Label(window, text="Enter Age:")
+age_label.pack()
+
+age_entry = tk.Entry(window)
+age_entry.pack()
+
+work_label = tk.Label(window, text="Work: ", font=("Arial", 12))
+work_label.pack()
+
+# Create a button to assign work
+assign_button = tk.Button(window, text="Assign Work", command=assign_work)
+assign_button.pack()
+
+# Run the tkinter main loop
+window.mainloop()
